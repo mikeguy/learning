3ecf946518704ba97e5e0e3a00fad2b896083c8d
Mike S <mikeguy@users.noreply.github.com>
2023-09-19 20:06:23 -0400

    app testing

---
+++ mobile/sessions_iOS.py
@@ -0,0 +1,38 @@
+from appium import webdriver
+from appium.options.ios import XCUITestOptions
+
+#need to teach our script where the app is located
+from os import path
+
+#absolute path to directory where app is, variable, __file__ is a magic variable where the file is 
+#encountered
+CUR_DIR = path.dirname(path.abspath(__file__))
+
+#get the actual path to application file (joins the different path segments, 
+#then we get the full path dir and file)
+APP = path.join(CUR_DIR, 'TheApp.app.zip')
+
+#location of appium server, where is it listening for requests (we need to start Appium server from terminal)
+# run appium command in Terminal
+APPIUM = "http://localhost:4723"
+
+#define capabilities dictonary
+#CAPS = {
+#    "platformName": "iOS",
+#    "appium:options": {
+#        "platformVersion": "16.4",
+#        "deviceName": "iPhone 14",
+#        "automationName": "XCUITest",
+#        "app": APP
+#  }
+#}
+
+options = XCUITestOptions()
+options.platform_name= "iOS"
+options.device_name= "iPhone 14"
+options.automation_name = "XCUITest"
+options.app = APP
+
+
+driver = webdriver.Remote(command_executor=APPIUM, options=options)
+driver.quit()
