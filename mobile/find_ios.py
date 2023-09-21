da8d433558aa68da31c4d1738feb56d858ad5ee2
Mike S <mikeguy@users.noreply.github.com>
2023-09-21 11:58:07 -0400

    Update find_ios.py

--- mobile/find_ios.py
+++ mobile/find_ios.py
@@ -44,12 +44,14 @@ driver = webdriver.Remote(APPIUM, options=options)
 try: 
     #we need to wait some time for all elements to load
     wait = WebDriverWait(driver, 10)
+
     #wait until the "Login Screen" accessibility ID element is loaded
-    wait.until(expected_conditions.presence_of_all_elements_located(AppiumBy.ACCESSIBILITY_ID,'Login Screen'))
+    wait.until(expected_conditions.presence_of_all_elements_located((AppiumBy.ACCESSIBILITY_ID,'Login Screen')))
+    
     #try to find element by it's class name (type) : UI class name for anything that is a static type element (simple text label)
     driver.find_element(AppiumBy.CLASS_NAME, 'XCUIElementTypeStaticText')
     
-    driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@label="Webview Demo"]")
+    driver.find_element(AppiumBy.XPATH, "//XCUIElementTypeOther[@label='Webview Demo']")
 
 finally:
     driver.quit()
