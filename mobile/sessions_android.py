a7e40062baa95553e06e3a4eafbbf8df76912ae4
Mike S <mikeguy@users.noreply.github.com>
2023-09-21 08:04:35 -0400

    added android

--- mobile/sessions_android.py
+++ mobile/sessions_android.py
@@ -1,5 +1,6 @@
 from appium import webdriver
-from appium.options.ios import XCUITestOptions
+from appium.options.android import UiAutomator2Options
+
 
 #need to teach our script where the app is located
 from os import path
@@ -11,7 +12,7 @@ CUR_DIR = path.dirname(path.abspath(__file__))
 
 #get the actual path to application file (joins the different path segments, 
 #then we get the full path dir and file)
-APP = path.join(CUR_DIR, 'TheApp.app.zip')
+APP = path.join(CUR_DIR, 'TheApp.apk')
 
 #print(APP)
 #location of appium server, where is it listening for requests (we need to start Appium server from terminal)
@@ -20,11 +21,11 @@ APPIUM = "http://localhost:4723"
 
 #define capabilities dictonary
 appium_server_url = 'http://localhost:4723'
-options = XCUITestOptions()
-options.platformName = "iOS"
-options.platformVersion = "16.4"
-options.deviceName = "iPhone 14"
-options.automationName = "XCUITest"
+options = UiAutomator2Options()
+options.platformName = "Android"
+#options.platformVersion = "13"
+#options.deviceName = "Appium Test"
+options.automationName = "UiAutomator2"
 options.app = APP
 
 driver = webdriver.Remote(APPIUM, options=options)
